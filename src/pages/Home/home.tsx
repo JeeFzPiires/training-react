88c0df2754b5861d5ac1243f196ceababae1fac3
JeeFzPiires <jeferson.souza@easymine.com.br>
2024-02-22 13:53:17 -0300

    feat: criando projeto

---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,9 @@
+export const HomePage = () => {
+  return (
+    <div className="bg-slate-800 h-screen w-screen">
+      <header className="bg-slate-600 border-b-2 border-black h-20">
+        <div></div>
+      </header>
+    </div>
+  );
+};
